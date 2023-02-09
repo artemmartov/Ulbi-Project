38b2d762dc9eeeddb3f48ef02fa35f53afecad94
GEROYAN Artem <you@example.com>
2023-02-09 13:02:50 +0300

    add css loaders with css module and without

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const App = () => {
+    return (
+        <div className='app'>
+            <div>adfsdfsd33</div>
+        </div>
+    );
+};
+
+export default App;
